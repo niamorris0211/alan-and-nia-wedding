e234b46f01cebaf620ccc0c1c764a8b57c2c42bb
Nia Eleri Morris <niamorris0211@mac.home>
2026-05-14 17:04:46 +0100

    Use Formspree RSVP endpoint

--- rsvp-config.js
+++ rsvp-config.js
@@ -1,6 +1,6 @@
 window.RSVP_CONFIG = {
   emailSubmitEndpoint: "https://formsubmit.co/niamorris0211@hotmail.co.uk",
   googleAppsScriptUrl: "",
-  formspreeEndpoint: "",
+  formspreeEndpoint: "https://formspree.io/f/xwvydezz",
   fallbackEmail: "niamorris0211@hotmail.co.uk",
 };
